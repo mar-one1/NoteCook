b5095ce7cd6a3ce93d1b2023885a265aa57a31a7
mar-one <marwane.rays@gmail.Com>
2024-04-23 22:32:06 +0200

    fix handle error by sweetalert in ErrorHandler.java

--- app/src/main/java/com/example/notecook/Api/ApiClient.java
+++ app/src/main/java/com/example/notecook/Api/ApiClient.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 //    public static final String BASE_URL = "http://192.168.60.36:3000/";
-    public static final String BASE_URL = "https://6f96-41-248-126-193.ngrok-free.app/";
+    public static final String BASE_URL = "https://d05d-196-75-150-121.ngrok-free.app/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
--- app/src/main/java/com/example/notecook/Repo/ErrorHandler.java
+++ app/src/main/java/com/example/notecook/Repo/ErrorHandler.java
@@ -19,25 +19,35 @@ import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.net.SocketTimeoutException;
+import java.util.HashSet;
+import java.util.Set;
 
 import retrofit2.Response;
 
 public class ErrorHandler {
+    private static Set<String> errorMessages = new HashSet<>();
     public static void handleErrorResponse(Response<?> response, Activity appCompatActivity) {
         int statusCode = response.code();
         String message = response.message();
         if (response.errorBody() != null) {
-                if (Constants.alertDialog != null && Constants.alertDialog.isShowing() && alertDialog.getTitleText().equals(String.valueOf(statusCode))) {
-                    Constants.alertDialog.dismiss();
-                    return;
-                }
+            if (errorMessages.contains(message)) {
+                // If the error message is already in the set, return
+                return;
+            } else {
+                // Add the error message to the set
+                errorMessages.add(message);
+            }
+
+            if (Constants.alertDialog != null && Constants.alertDialog.isShowing() && alertDialog.getTitleText().equals(String.valueOf(statusCode))) {
+                Constants.alertDialog.dismiss();
+                return;
+            }
+
             if (statusCode == 400) {
                 try {
                     String errorBody = response.errorBody().string();
                     Gson gson = new Gson();
                     ValidationError validationError = gson.fromJson(errorBody, ValidationError.class);
-                    // Now you have the validation errors in the validationError object
-                    // Handle them accordingly
                     StringBuilder errorMessages = new StringBuilder();
                     for (ValidationError.ValidationErrorItem error : validationError.getErrors()) {
                         errorMessages.append(", ").append(error.getMessage());
@@ -46,24 +56,17 @@ public class ErrorHandler {
                 } catch (IOException e) {
                     // Handle error parsing error body
                 }
-                // Unauthorized, handle accordingly (e.g., reauthentication).
             } else if (statusCode == 409) {
                 Constants.AffichageMessage("Record already exists", "409", appCompatActivity);
-                // Unauthorized, handle accordingly (e.g., reauthentication).
             } else if (statusCode == 404) {
-                // Not found, handle accordingly (e.g., show a 404 error message).
                 Constants.AffichageMessage(TAG_NOT_FOUND, "404", appCompatActivity);
             } else if (statusCode == 401) {
-                // Not found, handle accordingly (e.g., show a 404 error message).
                 Constants.AffichageMessage(TAG_INFO_ERONEE, "401", appCompatActivity);
             } else if (statusCode == 500) {
-                // Handle other status codes or generic error handling.
                 Constants.AffichageMessage(TAG_ERREUR_SYSTEM, "500", appCompatActivity);
             } else if (statusCode == 406) {
-                // Handle other status codes or generic error handling.
                 Constants.AffichageMessage(TAG_PAS_RESULTAT, "406", appCompatActivity);
             } else if (statusCode == 502) {
-                // Handle other status codes or generic error handling.
                 Constants.AffichageMessage(TAG_SERVEUR_HORS_SERVICE, "502", appCompatActivity);
             } else
                 Constants.AffichageMessage(TAG_CONNEXION_MESSAGE, "message", appCompatActivity);
@@ -72,18 +75,12 @@ public class ErrorHandler {
     }
 
     public static void handleNetworkFailure(Throwable t, Context context) {
-        // Handle network failure
         TAG_CONNEXION_MESSAGE = t.toString();
-        // Handle network failure or timeout
         if (t instanceof SocketTimeoutException) {
-            // Handle timeout exception
             System.out.println("Timeout occurred");
         } else {
-            // Handle other network failures
             t.printStackTrace();
         }
-        //Constants.AffichageMessage(TAG_CONNEXION_MESSAGE, context);
         Toast.makeText(context, TAG_CONNEXION_MESSAGE, Toast.LENGTH_SHORT).show();
     }
-
 }
