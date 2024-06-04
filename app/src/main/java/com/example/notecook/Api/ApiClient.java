96ab61b291f938c042d5ac823e28a59002f8a78a
mar-one <marwane.rays@gmail.Com>
2024-06-04 22:16:09 +0200

    init commit

--- app/src/main/java/com/example/notecook/Api/ApiClient.java
+++ app/src/main/java/com/example/notecook/Api/ApiClient.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 //        public static final String BASE_URL = "http://192.168.56.1:3000/";
-        public static final String BASE_URL = "https://515f-102-100-236-65.ngrok-free.app/";
+        public static final String BASE_URL = "https://3180-105-157-8-63.ngrok-free.app/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
