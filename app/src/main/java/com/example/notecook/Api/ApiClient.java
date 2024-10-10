e86e6b404ecab563e8a8d64b6f1d4ae7e77675d6
marwane <marwane.rays@gmail.com>
2024-10-10 14:54:24 +0100

    fix add recipe design ...

--- app/src/main/java/com/example/notecook/Api/ApiClient.java
+++ app/src/main/java/com/example/notecook/Api/ApiClient.java
@@ -14,7 +14,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static final String BASE_URL = "http://192.168.0.157:3000/";
+    public static final String BASE_URL = "http://192.168.56.1:3000/";
 //     public static final String BASE_URL = "https://9254-196-64-117-33.ngrok-free.app/";
     private static Retrofit retrofit = null;
 
