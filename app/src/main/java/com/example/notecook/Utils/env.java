822784f85a9ff608fc7895384a1c468255c07bef
Your Name <your.email@example.com>
2024-10-17 14:51:12 +0100

    Add file to .gitignore

--- app/src/main/java/com/example/notecook/Utils/env.java
+++
@@ -1,7 +0,0 @@
-package com.example.notecook.Utils;
-
-public class env {
-    public static final String URL = "192.168.56.1";
-    public static final String PORT = "3000";
-    public static final String BASE_URL = "http://"+URL+":"+PORT+"/";
-}
