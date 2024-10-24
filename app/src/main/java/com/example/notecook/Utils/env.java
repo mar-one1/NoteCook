6a6bf6690803ee1a56aed319db735302a0ea216b
Your Name <your.email@example.com>
2024-10-24 21:06:07 +0200

    make update recipe from local with image remote and local ,but delete old image not yet fixed !!!

--- app/src/main/java/com/example/notecook/Utils/env.java
+++ app/src/main/java/com/example/notecook/Utils/env.java
@@ -3,5 +3,5 @@ package com.example.notecook.Utils;
 public class env {
     public static final String IP = "192.168.56.1";
     public static final String PORT = "3000";
-    public static final String BASE_URL = "http://"+IP+":"+PORT+"/";
+    public static final String BASE_URL = "http://" + IP + ":" + PORT + "/";
 }
